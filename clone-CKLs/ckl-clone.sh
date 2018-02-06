459bd6c19719ad4cf4d1fc239c619f7428c3c074
Jacob Cutright <cutright_jm@wvwc.edu>
2018-02-05 23:14:50 -0500

    Create ckl-clone.sh

---
+++ clone-CKLs/ckl-clone.sh
@@ -0,0 +1,15 @@
+if [ $# -eq 0 ]; then
+    echo "Script requires two arguments: ./duplicate.sh <comps> <golden>"
+    echo "   <comps> is a list of hostnames"
+    echo "   <golden> is a folder containing the files"
+    exit 1
+fi
+
+comps="$1"
+golden="$2"
+
+mkdir results
+
+< $comps xargs -I% bash -c 'mkdir ./results/% && cp -r ./'"$golden"'/* ./results/%'
+< $comps xargs -IcurrentHost bash -c 'find ./results/currentHost -iname "*$golden*" -exec rename '"$golden"' currentHost {} \;'
+< $comps xargs -IcurrentHost bash -c 'find ./results/currentHost -type f -exec sed -i 's/"$golden"/'currentHost'/' {} \;'
